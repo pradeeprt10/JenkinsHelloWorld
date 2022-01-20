9f699475084a9a52e5231b52c01129e327a01819
pradeeprt10 <51189427+pradeeprt10@users.noreply.github.com>
2022-01-20 22:57:52 +0530

    Update HelloWorld.java
    2nd Time Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld{
 
 public static void main(String[] args){
 
-System.out.println("*This is Hello World programme for jenkins*");
+System.out.println("*This is Hello World for jenkins by Pradeep*");
+
 }
 
 }
